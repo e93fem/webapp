51a843151a4d638014317e3c92d2f4655047bd2c
Fredrik Emilsson <fredrik.emilsson@live.se>
2025-01-24 14:26:18 +0100

    completed the read expenses

---
+++ src/components/ExpenceList.tsx
@@ -0,0 +1,28 @@
+import { Expense } from "../model/Expense";
+
+interface Props {
+    expenses: Expense[]
+}
+
+const ExpenceList = ({expenses}: Props) => {
+        return <div>
+            <table border={1}>
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Amount</th>
+                        <th>Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {expenses.map(expense => <tr key={expense.expenseId}>
+                        <td>{expense.name}</td>
+                        <td>{expense.amount}</td>
+                        <td>{expense.date}</td>
+                        </tr>)}
+                </tbody>
+            </table>
+        </div>
+}
+
+export default ExpenceList
