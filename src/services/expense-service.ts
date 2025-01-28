f2e63659ef6b86404af2931fcf0618fb26da0ec9
Fredrik Emilsson <fredrik.emilsson@live.se>
2025-01-28 15:48:09 +0100

    Completed read by expense id

--- src/services/expense-service.ts
+++ src/services/expense-service.ts
@@ -2,5 +2,9 @@ import apiClient from "../config/api-client"
 import { Expense } from "../model/Expense"
 
 export const getExpenses = () => {
-    return apiClient.get<Expense[]>('/expenses')
+    return apiClient.get<Expense[]>('/expenses');
+}
+
+export const getExpenseByExpenseId = (expenseId: string) => {
+    return apiClient.get<Expense>(`/expenses/${expenseId}`);
 }
